37df35a8e3605d436f4ed5d4741727e16d2041cf
Ika Yébenes Herce <ikayherce@gmail.com>
2024-03-26 13:38:26 +0000

    added post_detail view

--- my_project/settings.py
+++ my_project/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False   
+DEBUG = False    
 
 ALLOWED_HOSTS = ['8000-ikayherce-debbiesblog-6l8jg7btuxf.ws-eu110.gitpod.io','.herokuapp.com'
 ]
