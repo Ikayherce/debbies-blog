6c4e5899a3ec532070e96c2d14957acd770d4119
Ika Yébenes Herce <ikayherce@gmail.com>
2024-03-26 17:55:45 +0000

    add about app

--- my_project/settings.py
+++ my_project/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False    
+DEBUG = True    
 
 ALLOWED_HOSTS = ['8000-ikayherce-debbiesblog-6l8jg7btuxf.ws-eu110.gitpod.io','.herokuapp.com'
 ]
@@ -44,8 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', 
-    'django_summernote',
     'blog',
+    'django_summernote',
+    'about',
 ]
 
 MIDDLEWARE = [
