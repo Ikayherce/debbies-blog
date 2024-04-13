92c03a1a67eb096fd96281d4159d1875c1186494
Ika Yébenes Herce <ikayherce@gmail.com>
2024-04-13 20:26:28 +0000

    change order of url patterns to fix bug

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('add_new_post/', AddPostView.as_view(), name='add_new_post'),  #test code for new post view
     path('add_category/', AddCategoryView.as_view(), name='add_category'),  #test code for new post view
-    path('<slug:slug>/', views.post_detail, name='post_detail'),  
     path('category-list/',CategoryListView, name= 'category-list'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),  
     path('<slug:slug>/edit_comment/<int:comment_id>',
         views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
